d9d0aefd7349d642db16a66856dfdf8c661fd328
wellingtonhiago <wellingtonhiago2.0@gmail.com>
2022-03-25 21:03:15 -0300

    Business Card Criado

---
+++ app/src/main/java/br/com/dio/businesscard/ui/AddBusinessCardActivity.kt
@@ -0,0 +1,11 @@
+package br.com.dio.businesscard.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddBusinessCardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_business_card)
+    }
+}
